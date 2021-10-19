3515045725eb0fd3fc77e4a9cec0a9b325748c54
Elliot Robinson <elliot.robinson@argiopetech.com>
2021-10-19 19:37:29 -0400

    Add body of remove-task.sh

--- remove-task.sh
+++ remove-task.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo helle world
+if [ -z "$1" ]; then
+    echo "$0 <number of task list item to remove>"
+    exit
+fi
+
+if [ "$1" -le 0 ]; then
+    echo "Invalid option"
+    exit
+fi
+
+sed -i "$1 d" ~/.todo/tasks.txt
